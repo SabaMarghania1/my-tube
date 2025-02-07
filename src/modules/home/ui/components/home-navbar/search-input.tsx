1157cdb905ce59cf86ef9082cff15259b5e14c20
saba Marghania <marghanias@gmail.com>
2025-02-07 04:41:40 +0400

    Finish Basic Layout

---
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -0,0 +1,23 @@
+import { SearchIcon } from "lucide-react";
+
+export const SearchInput = () => {
+  // Todo  Search functionality
+  return (
+    <form className="flex w-full max-w-[600px]">
+      <div className="relative w-full">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full pl-4 py-2 pr-12 rounded-l-full border focus:outline-none focus:border-blue-500"
+        />
+        {/* TOdo: add remove search button */}
+      </div>
+      <button
+        type="submit"
+        className="px-5 py-2.5 bg-gray-100 border-l-0 rounded-r-full hover:bg-gray-200 disabled:opacity-50 cursor-not-allowed"
+      >
+        <SearchIcon size={20} />
+      </button>
+    </form>
+  );
+};
